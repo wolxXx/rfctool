080316a0e14c33ce355f3abd9aff56019e6ba0ea
wolxXx <devops@wolxxx.de>
2025-03-04 00:11:36 +0100

    Set up Slim framework with PHP-DI container
    Initialized the Slim framework using PHP-DI for dependency injection. Added routing and error middleware, alongside a basic route definition. This sets up the foundation for the application structure.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,40 @@
-hello, world
+<?php
+
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+// Autoloader
+require __DIR__ . '/vendor/autoload.php';
+
+// Create Container using PHP-DI
+$containerBuilder = new ContainerBuilder();
+
+// Add Container Definitions
+$containerBuilder->addDefinitions([
+    // Add any dependencies here, if required
+]);
+
+// Build Container
+$container = $containerBuilder->build();
+
+// Set App Factory Container
+AppFactory::setContainer($container);
+
+// Create App
+$app = AppFactory::create();
+
+// Define Routing Middleware
+$app->addRoutingMiddleware();
+
+// Error Middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Define Routes
+$app->get('/', \App\Action\HomeAction::class . ':handle');
+
+// Run App
+$app->run();
