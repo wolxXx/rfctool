cec920b17b5bfbf3b278e34b105120a1cca8deb9
wolxXx <devops@wolxxx.de>
2025-03-04 00:11:26 +0100

    Add User entity with basic fields and getters/setters
    Introduced a new User entity with Doctrine ORM annotations. The entity includes fields for ID, name, email, createdBy, and updatedBy, along with corresponding getter and setter methods. This serves as the foundation for managing user data in the system.

---
+++ src/Model/User.php
@@ -0,0 +1,79 @@
+<?php
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "entity_name")]
+class EntityName
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private int $id;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: "string", length: 255, unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $createdBy;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $updatedBy;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?string $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getUpdatedBy(): ?string
+    {
+        return $this->updatedBy;
+    }
+
+    public function setUpdatedBy(?string $updatedBy): self
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+}
