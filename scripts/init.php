417fef825589dac43d78cf4b85e9532ab823ec88
wolxXx <devops@wolxXx.de>
2025-03-07 09:19:09 +0100

    Refactor init command and add user initialization check.
    Split logic into a new `checkUsers` method to handle user initialization. Automatically creates a default admin user if no users exist and outputs appropriate messages. Improved code structure and readability while maintaining functionality.

--- scripts/init.php
+++ scripts/init.php
@@ -4,8 +4,10 @@ require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 #[\Symfony\Component\Console\Attribute\AsCommand(name: 'app:init')]
 
-class Command extends Symfony\Component\Console\Command\Command {
-    private \Symfony\Component\Console\Input\InputInterface $input;
+class Command extends
+    Symfony\Component\Console\Command\Command
+{
+    private \Symfony\Component\Console\Input\InputInterface   $input;
 
     private \Symfony\Component\Console\Output\OutputInterface $output;
 
@@ -20,25 +22,51 @@ class Command extends Symfony\Component\Console\Command\Command {
         $this->output = $output;
         $this->output->writeln('Hello, World!');
 
+        $this->checkUsers();
 
-        $usersCount = \RfcTool\Entity\User::getRepository()->count();
+
+        return static::SUCCESS;
+    }
+
+    protected function checkUsers(): void
+    {
+
+        $usersCount = \RfcTool\Entity\User::getRepository()
+                                          ->count()
+        ;
 
         if (0 !== $usersCount) {
             $this->output->writeln('There are ' . $usersCount . ' users');
+
+            return;
         }
-        if (0 === $usersCount) {
-            $this->output->writeln('There are no users yet, creating first admin user.');
+        $this->output->writeln('There are no users yet, creating first admin user.');
 
-        }
+        $newUser = new \RfcTool\Service\User\Creator()
+            ->do(
+                new \RfcTool\Service\User\Creator\Bag()
+                    ->setEmail('rfc@rfc.tool')
+                    ->setPersist(true)
+                    ->setName('RfcToolAdmin')
+                    ->setRole(\RfcTool\Definition\User\Role::admin)
+                    ->setStatus(\RfcTool\Definition\User\Status::active)
+                    ->setCredentials(
+                        new \RfcTool\Service\User\Creator\CredentialList()
+                            ->push(
+                                new \RfcTool\Service\User\Creator\CredentialBag()
+                                    ->setType(\RfcTool\Definition\User\CredentialType::password)
+                                    ->setPassword('initial password'),
+                            ),
+                    ),
+            )
+        ;
 
-        \RfcTool\Util\Debugger::dieDebug($usersCount);
-
-        return static::SUCCESS;
+        \RfcTool\Util\Debugger::dieDebug($newUser->getId(), $newUser->getName());
     }
 }
 
 
-$command = new Command();
+$command     = new Command();
 $application = new \Symfony\Component\Console\Application();
 $application->add($command);
 $application->setDefaultCommand($command->getName());
