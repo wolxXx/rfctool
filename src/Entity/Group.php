e42db1b473934947f48ed0752ff9d440255fa65b
wolxXx <devops@wolxXx.de>
2025-03-11 17:45:37 +0100

    Add getter and setter methods for Group entity
    Introduced getter and setter methods for `name` and `description` properties to improve data handling and encapsulation. Also added a getter method for users to return the `ArrayCollection` of associated users.

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace RfcTool\Entity;
 
-
 #[\Doctrine\ORM\Mapping\Entity(
     repositoryClass: Group\Repository::class
 )]
@@ -19,36 +18,67 @@ class Group
 
     public const string TABLE_NAME = 'group';
 
-
     #[\Doctrine\ORM\Mapping\Column(
         type  : \Doctrine\DBAL\Types\Types::STRING,
         length: 255
     )]
-    protected string     $name;
+    protected string  $name;
 
     #[\Doctrine\ORM\Mapping\Column(
-        type  : \Doctrine\DBAL\Types\Types::TEXT,
-        length: 255,
+        type    : \Doctrine\DBAL\Types\Types::TEXT,
+        length  : 255,
         nullable: true
     )]
-    protected ?string     $description = null;
+    protected ?string $description = null;
 
     /**
      * Many Users have Many Groups.
+     *
      * @var \Doctrine\Common\Collections\Collection<int, Group>
      */
     #[\Doctrine\ORM\Mapping\ManyToMany(targetEntity: User::class, inversedBy: 'groups')]
     #[\Doctrine\ORM\Mapping\JoinTable(name: 'users_groups')]
     private \Doctrine\Common\Collections\Collection $users;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->users = new \Doctrine\Common\Collections\ArrayCollection();
     }
-
 
     public static function getRepository(): Group\Repository
     {
         return static::getRepositoryByClassName();
     }
 
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getUsers(): array
+    {
+        return $this->users;
+    }
 }
