998e24b6c6f1d727908d317633f13da2071ee59a
wolxXx <devops@wolxxx.de>
2025-03-13 17:36:56 +0100

    Refactor Result class to improve readability and modularity
    Reorganized methods and introduced helper functions to break down responsibilities, improving code readability and maintainability. Modularized calculations and vote counting logic. Removed redundant code by encapsulating shared operations into dedicated methods.

--- src/Service/Proposal/Result.php
+++ src/Service/Proposal/Result.php
@@ -4,59 +4,42 @@ namespace RfcTool\Service\Proposal;
 
 class Result
 {
+    protected \RfcTool\Entity\Proposal $proposal;
+
     protected \RfcTool\Definition\Proposal\Result $result;
 
-    protected int                                 $allowed       = 0;
+    protected int $allowed = 0;
 
-    protected int                                 $notVoted      = 0;
+    protected int $notVoted = 0;
 
-    protected int                                 $notVotedQuote = 0;
+    protected int $notVotedQuote = 0;
 
-    protected int                                 $votedQuote    = 0;
+    protected int $votedQuote = 0;
 
-    protected int                                 $pro           = 0;
+    protected int $pro = 0;
 
-    protected int                                 $contra        = 0;
+    protected int $contra = 0;
 
-    protected int                                 $none          = 0;
+    protected int $none = 0;
 
-    protected int                                 $proQuote      = 0;
+    protected int $proQuote = 0;
 
-    protected int                                 $contraQuote   = 0;
+    protected int $contraQuote = 0;
 
-    protected int                                 $noneQuote     = 0;
+    protected int $noneQuote = 0;
 
-    protected int                                 $voteCount     = 0;
+    protected int $voteCount = 0;
 
     public function get(\RfcTool\Entity\Proposal $proposal): static
     {
-        $group = $proposal->getGroup();
-        if (null === $group) {
+        $this->proposal = $proposal;
+        if (true === $this->isProposalOpen()) {
             $this->result = \RfcTool\Definition\Proposal\Result::OPEN;
 
             return $this;
         }
 
-        if (null === $proposal->getVoteStart() || null === $proposal->getVoteEnd()) {
-            $this->result = \RfcTool\Definition\Proposal\Result::OPEN;
-
-            return $this;
-        }
-
-        $usersInGroupRaw = \RfcTool\Entity\UserInGroup::getRepository()->getForGroup($group);
-        $usersInGroup    = [];
-
-        $votePeriod = new \DatePeriod(start: $proposal->getVoteStart(), interval: new \DateInterval('P1D'), end: $proposal->getVoteEnd(), options: \DatePeriod::INCLUDE_END_DATE);
-        foreach ($usersInGroupRaw as $userInGroup) {
-            foreach ($votePeriod as $dayInPeriod) {
-                if ($userInGroup->getBeginDate() <= $dayInPeriod && (null === $userInGroup->getEndDate() || $dayInPeriod <= $userInGroup->getEndDate())) {
-                    $usersInGroup[$userInGroup->getUser()->getId()] = $userInGroup->getUser();
-                    continue 2;
-                }
-            }
-        }
-
-        $this->allowed = \count($usersInGroup);
+        $this->countAllowed();
 
         if (0 === $this->allowed) {
             $this->result = \RfcTool\Definition\Proposal\Result::ILLEGAL;
@@ -64,13 +47,66 @@ class Result
             return $this;
         }
 
-        $votes = \RfcTool\Entity\Proposal\Vote::getRepository()
-                                              ->createQueryBuilder('vote')
-                                              ->andWhere('vote.proposal = :proposal')
-                                              ->setParameter('proposal', $proposal)
-                                              ->getQuery()
-                                              ->execute()
-        ;
+        $this->countVotes();
+
+        if (0 === $this->voteCount) {
+            $this->result = \RfcTool\Definition\Proposal\Result::OPEN;
+
+            return $this;
+        }
+
+        return $this
+            ->grabQuotes()
+            ->calculateResult($proposal->getCalculationBase())
+            ;
+    }
+
+
+    private function calculateResult(\RfcTool\Definition\Proposal\CalculationBase $calculationBase): static
+    {
+        return match ($calculationBase) {
+            \RfcTool\Definition\Proposal\CalculationBase::ALL => $this->calculateOnAllBase(),
+            \RfcTool\Definition\Proposal\CalculationBase::ALL_PRESENT => $this->calculateOnAllPresentBase(),
+            \RfcTool\Definition\Proposal\CalculationBase::SIMPLE_MAJORITY => $this->calculateOnSimpleMajorityBase(),
+            \RfcTool\Definition\Proposal\CalculationBase::MAJORITY => $this->calculateOnMajorityBase(),
+            \RfcTool\Definition\Proposal\CalculationBase::TWO_THIRDS => $this->calculateOnTwoThirdsBase(),
+            \RfcTool\Definition\Proposal\CalculationBase::THREE_QUARTERS => $this->calculateOnThreeQuartersBase(),
+            default => throw new \InvalidArgumentException('Unknown calculation base: ' . $calculationBase->value)
+        };
+    }
+
+
+    protected function isProposalOpen(): bool
+    {
+        return
+                null === $this->proposal->getGroup()
+            ||  null === $this->proposal->getVoteStart()
+            ||  null === $this->proposal->getVoteEnd()
+            ;
+    }
+
+
+    protected function getVotePeriod(): \DatePeriod
+    {
+        return new \DatePeriod(start: $this->proposal->getVoteStart(), interval: new \DateInterval('P1D'), end: $this->proposal->getVoteEnd(), options: \DatePeriod::INCLUDE_END_DATE);
+    }
+
+    /**
+     * @return \RfcTool\Entity\Proposal\Vote[]
+     */
+    protected function getVotes(): array
+    {
+        return \RfcTool\Entity\Proposal\Vote::getRepository()
+            ->createQueryBuilder('vote')
+            ->andWhere('vote.proposal = :proposal')
+            ->setParameter('proposal', $this->proposal)
+            ->getQuery()
+            ->execute();
+    }
+
+    protected function countVotes(): static
+    {
+        $votes = $this->getVotes();
         foreach ($votes as $vote) {
             $this->voteCount++;
             if (true === $vote->getVoice()) {
@@ -84,68 +120,89 @@ class Result
             $this->none++;
         }
 
-        if (0 === $this->voteCount) {
-            $this->result = \RfcTool\Definition\Proposal\Result::OPEN;
+        return $this;
+    }
+
+    protected function grabQuotes(): static
+    {
+        $this->proQuote = (int)\floor(100 / $this->voteCount) * $this->pro;
+        $this->contraQuote = (int)\floor(100 / $this->voteCount) * $this->contra;
+        $this->noneQuote = (int)\floor(100 / $this->voteCount) * $this->none;
+        $this->notVoted = $this->allowed - $this->voteCount;
+        $this->notVotedQuote = (int)\floor(100 / $this->allowed) * ($this->allowed - $this->voteCount);
+        $this->votedQuote = (int)\floor(100 / $this->allowed) * $this->voteCount;
+
+        return $this;
+    }
+
+    protected function countAllowed(): static
+    {
+        $usersInGroupRaw = \RfcTool\Entity\UserInGroup::getRepository()->getForGroup($this->proposal->getGroup());
+        $usersInGroup = [];
+        $votePeriod = $this->getVotePeriod();
+
+        foreach ($usersInGroupRaw as $userInGroup) {
+            foreach ($votePeriod as $dayInPeriod) {
+                if ($userInGroup->getBeginDate() <= $dayInPeriod && (null === $userInGroup->getEndDate() || $dayInPeriod <= $userInGroup->getEndDate())) {
+                    $usersInGroup[$userInGroup->getUser()->getId()] = $userInGroup->getUser();
+                    continue 2;
+                }
+            }
+        }
+
+        $this->allowed = \count($usersInGroup);
+
+        return $this;
+    }
+
+    protected function calculateOnAllBase(): static
+    {
+        $this->result = $this->allowed !== $this->pro ? \RfcTool\Definition\Proposal\Result::REJECTED : \RfcTool\Definition\Proposal\Result::ACCEPTED;
+
+        return $this;
+    }
+
+    protected function calculateOnAllPresentBase(): static
+    {
+        $this->result = $this->voteCount !== $this->pro ? \RfcTool\Definition\Proposal\Result::REJECTED : \RfcTool\Definition\Proposal\Result::ACCEPTED;
+
+        return $this;
+    }
+
+    protected function calculateOnSimpleMajorityBase(): static
+    {
+        if ($this->pro === $this->contra) {
+            $this->result = \RfcTool\Definition\Proposal\Result::DRAW;
 
             return $this;
         }
 
-        $this->proQuote      = (int)\floor(100 / $this->voteCount) * $this->pro;
-        $this->contraQuote   = (int)\floor(100 / $this->voteCount) * $this->contra;
-        $this->noneQuote     = (int)\floor(100 / $this->voteCount) * $this->none;
-        $this->notVoted      = $this->allowed - $this->voteCount;
-        $this->notVotedQuote = (int)\floor(100 / $this->allowed) * ($this->allowed - $this->voteCount);
-        $this->votedQuote    = (int)\floor(100 / $this->allowed) * $this->voteCount;
+        $this->result = $this->pro > $this->contra ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
 
-        switch ($proposal->getCalculationBase()) {
-            case \RfcTool\Definition\Proposal\CalculationBase::ALL:
-            {
-                $this->result = $this->allowed !== $this->pro ? \RfcTool\Definition\Proposal\Result::REJECTED : \RfcTool\Definition\Proposal\Result::ACCEPTED;
-
-                return $this;
-            }
-            case \RfcTool\Definition\Proposal\CalculationBase::ALL_PRESENT:
-            {
-                $this->result = $this->voteCount !== $this->pro ? \RfcTool\Definition\Proposal\Result::REJECTED : \RfcTool\Definition\Proposal\Result::ACCEPTED;
-
-                return $this;
-            }
-            case \RfcTool\Definition\Proposal\CalculationBase::SIMPLE_MAJORITY:
-            {
-                if ($this->pro === $this->contra) {
-                    $this->result = \RfcTool\Definition\Proposal\Result::DRAW;
-
-                    return $this;
-                }
-
-                $this->result = $this->pro > $this->contra ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
-
-                return $this;
-            }
-            case \RfcTool\Definition\Proposal\CalculationBase::MAJORITY:
-            {
-                $this->result = $this->pro >= 50 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
-
-                return $this;
-            }
-            case \RfcTool\Definition\Proposal\CalculationBase::TWO_THIRDS:
-            {
-                $this->result = $this->pro >= 66 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
-
-                return $this;
-            }
-            case \RfcTool\Definition\Proposal\CalculationBase::THREE_QUARTERS:
-            {
-                $this->result = $this->pro >= 75 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
-
-                return $this;
-            }
-            default:
-            {
-                throw new \InvalidArgumentException('Unknown calculation base: ' . $proposal->getCalculationBase()->value);
-            }
-        }
+        return $this;
     }
+
+    protected function calculateOnMajorityBase(): static
+    {
+        $this->result = $this->pro >= 50 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
+
+        return $this;
+    }
+
+    protected function calculateOnTwoThirdsBase(): static
+    {
+        $this->result = $this->pro >= 66 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
+
+        return $this;
+    }
+
+    protected function calculateOnThreeQuartersBase(): static
+    {
+        $this->result = $this->pro >= 75 ? \RfcTool\Definition\Proposal\Result::ACCEPTED : \RfcTool\Definition\Proposal\Result::REJECTED;
+
+        return $this;
+    }
+
 
     public function getResult(): \RfcTool\Definition\Proposal\Result
     {
