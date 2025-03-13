6551b447e5b46b223a3eecc74f800d260696abc6
wolxXx <devops@wolxxx.de>
2025-03-13 17:36:14 +0100

    Fix Role enum formatting and correct duplicate values
    Aligned case formatting for consistency and rectified the duplicate 'manager' value in the Role::ALL and getRoles methods, replacing it with 'admin'. Improved code readability and ensured data accuracy.

--- src/Definition/User/Role.php
+++ src/Definition/User/Role.php
@@ -1,25 +1,25 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace RfcTool\Definition\User;
 
 
 enum Role: string
 {
-    case guest   = 'guest';
+    case guest = 'guest';
 
-    case user    = 'user';
+    case user = 'user';
 
     case manager = 'manager';
 
-    case admin   = 'admin';
+    case admin = 'admin';
 
     public const array ALL = [
         self::guest->value,
         self::user->value,
         self::manager->value,
-        self::manager->value,
+        self::admin->value,
     ];
 
     public static function getRoles(): array
@@ -28,7 +28,7 @@ enum Role: string
             self::guest->value,
             self::user->value,
             self::manager->value,
-            self::manager->value,
+            self::admin->value,
         ];
     }
 }
