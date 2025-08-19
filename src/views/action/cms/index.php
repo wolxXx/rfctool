831f471c1cbe0a379da6659363bf869b918271fd
wolxXx <devops@wolxxx.de>
2025-08-19 08:13:43 +0200

    Add typed variable declarations in view templates
    Introduced strict type declarations and variable annotations in `index.php` and `main.php` to enhance code clarity, ensure type safety, and improve static analysis.

--- src/views/action/cms/index.php
+++ src/views/action/cms/index.php
@@ -1,1 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+/**
+ * @var \Slim\Views\PhpRenderer   $this
+ * @var string                    $currentRoute
+ * @var string                    $currentRoutePattern
+ * @var \RfcTool\Entity\User|null $currentUser
+ * @var \RfcTool\Util\ErrorStore  $errorStore
+ * @var \RfcTool\Util\Translator  $translator
+ */
+
+?>
+
+
 hallo, welt!
--- src/views/layout/main.php
+++ src/views/layout/main.php
@@ -1,3 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+/**
+ * @var \Slim\Views\PhpRenderer   $this
+ * @var string                    $currentRoute
+ * @var string                    $currentRoutePattern
+ * @var \RfcTool\Entity\User|null $currentUser
+ * @var \RfcTool\Util\ErrorStore  $errorStore
+ * @var \RfcTool\Util\Translator  $translator
+ */
+
+/**
+ * @var string $content
+ */
+
+?>
+
 <script src="/js/xdebug.js?v=<?= filemtime(filename: 'public/js/xdebug.js') ?>"></script>
 
 <?= $content ?>
